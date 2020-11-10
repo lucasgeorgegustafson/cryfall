51f1ac61c1845d79455f9e977b82c7c845398396
Lucas Gustafson <lucasgustafun@gmail.com>
2020-11-10 00:28:19 -0500

    wip: Decks endpoint

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+psycopg2://postgres:password@localhost:5432/cryfall'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Deck(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/decks")
+def list_decks():
+   return Deck.query.all() 
+
+
+
+
