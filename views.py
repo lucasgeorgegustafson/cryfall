4b573eb84668a0754d1e0d626e71929e390ed4d6
Lucas Gustafson <lucasgustafun@gmail.com>
2020-11-17 00:49:07 -0500

    fixed list_decks and refactoring

---
+++ views.py
@@ -0,0 +1,14 @@
+from models.deck import Deck
+from flask import Blueprint
+
+blueprint = Blueprint('decks', __name__)
+
+@blueprint.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@blueprint.route("/decks", methods = ('GET',))
+def list_decks():
+    decks = Deck.query.all()
+    deck_list = [{'id': deck.id, 'name': deck.name} for deck in decks]
+    return {'decks': deck_list} 
