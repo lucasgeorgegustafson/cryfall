1b1c147150f59878b1627a0c09f428456862fc9a
Ian Gustafson <ian@dispatch.me>
2020-11-24 23:45:04 -0500

    Working local config/development setup!
    * Add run script template
    * Move ENV config from config file to run template
    * Load config from file in main.py
    * Add comments in main.py
    * Update .gitignore

--- api/main.py
+++ api/main.py
@@ -4,8 +4,17 @@ from flask_migrate import Migrate
 from views import blueprint
 from database import db
 
+# initialize the flask app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+psycopg2://postgres:password@localhost:5432/cryfall'
+
+# load config values from the file at the path stored in an environment variable
+app.config.from_envvar('CRYFALL_CONFIG_FILE')
+
+# configure the app with a database connection
 db.init_app(app)
+
+# init the migration library
 migrate = Migrate(app, db)
+
+# load the views
 app.register_blueprint(blueprint)
