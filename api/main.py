5e1f8941f7e7a45294268e91c88a932721c280ab
Ian Gustafson <ian@dispatch.me>
2020-11-24 21:35:59 -0500

    Add db migrations directory with initial migration

--- api/main.py
+++ api/main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from views import blueprint
 from database import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+psycopg2://postgres:password@localhost:5432/cryfall'
 db.init_app(app)
+migrate = Migrate(app, db)
 app.register_blueprint(blueprint)
