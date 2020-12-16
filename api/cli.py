b0f5e142a703a0d6de92082ce2ffcdd667f7b196
Ian Gustafson <ian@dispatch.me>
2020-12-15 23:50:33 -0500

    Transitions and Event Handlers as Props (#6)
    * Refactor and fix transitions and styling
    
    * Make DeleteButton take an onClick function (separation of concerns)
    * Fix transition on deck delete
    * Switch to Collapse transition on deck delete
    * Spiffy new header
    
    * Improvements to DB seed
    
    * Now generates 25 decks
    * Now fakes deck name and format
    * Refactor to seed_decks()
    * Fix code style warnings
    
    * Ignore JetBrains IDE settings
    
    * Remove commented CSS
    
    * Remove import of unused icon
    
    * Rename state property to isDeleted

--- api/cli.py
+++ api/cli.py
@@ -1,15 +1,107 @@
+import itertools
+import random
 from database import db
 from models.deck import Deck
 
-def seed_db():
-    decks = [
-        Deck(name='Boros Cycling', format='standard'),
-        Deck(name='Gruul Adventures', format='standard'),
-        Deck(name='Esper Doom', format='standard'),
-        Deck(name='Dimir Rogues', format='standard'),
-        Deck(name='Mono Green Tron', format='modern')
-    ]
+deck_colors = [
+    'Colorless',
+    'Mono White',
+    'Mono Blue',
+    'Mono Black',
+    'Mono Red',
+    'Mono Green',
+    'WU',
+    'Azorius',
+    'UB',
+    'Dimir',
+    'BR',
+    'Rakdos',
+    'RG',
+    'Gruul',
+    'GW',
+    'Selesnya',
+    'WB',
+    'Orzhov',
+    'UR',
+    'Izzet',
+    'BG',
+    'Golgari',
+    'RW',
+    'Boros',
+    'WUG',
+    'Bant',
+    'WUB',
+    'Esper',
+    'UBR',
+    'Grixis',
+    'BRG',
+    'Jund',
+    'WRG',
+    'Naya',
+    'WBG',
+    'Abzan',
+    'WUR',
+    'Jeskai',
+    'UBG',
+    'Sultai',
+    'WBR',
+    'Mardu',
+    'URG',
+    'Temur',
+    '4c',
+    '5c'
+]
 
-    [ db.session.add(deck) for deck in decks ]
+deck_archetypes = [
+    'Aggro',
+    'Midrange',
+    'Control',
+    'Combo',
+    'Superfriends',
+    'Reanimator',
+    'Tempo',
+    'Ramp',
+    'Party',
+    'Landfall',
+    'Tron',
+    'Burn',
+    'Flash',
+    'Devotion',
+    'Sacrifice',
+    'Humans',
+    'Merfolk',
+    'Faeries',
+    'Goblins',
+    'Knights',
+    'Slivers'
+]
+
+deck_formats = [
+    'standard',
+    'historic',
+    'commander',
+    'brawl',
+    'pioneer',
+    'modern',
+    'pauper',
+    'legacy',
+    'vintage'
+]
+
+
+def seed_db():
+    return seed_decks()
+
+
+def seed_decks():
+    for _ in itertools.repeat(None, 25):
+        db.session.add(generate_deck())
 
     return db.session.commit()
+
+
+def generate_deck():
+    deck_name = random.choice(deck_colors) + ' ' + random.choice(deck_archetypes)
+    deck_format = random.choice(deck_formats)
+
+    return Deck(name=deck_name, format=deck_format)
